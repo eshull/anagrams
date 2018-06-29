83efc897b91639b98cbca46beded1a98ac612cd4
Eric Shull <eshulldesign@gmail.com>
2018-06-29 09:18:57 -0700

    add loops to take input string, split each letter and sort letters in each array

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,7 +4,15 @@ class Anagram
     @secondword = word2
   end
 
+
+
   def compare
+    word_array_one = @firstword.split("")
+    word_array_two = @secondword.split("")
+    sorted_one = word_array_one.sort do |a,b| a <=> b end
+    sorted_two = word_array_two.sort do |a,b| a <=> b end
+    print sorted_one
+    print sorted_two
     false
   end
 end
