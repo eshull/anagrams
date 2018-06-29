d7a04874591535ebee8ba8cb72152376d3e9559f
Eric Shull <eshulldesign@gmail.com>
2018-06-29 12:53:14 -0700

    add positive pass to third test

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,27 +4,39 @@ class Anagram
     @secondword = word2
   end
 
-  # def compare
-  #
-  #   word_array_one = @firstword.downcase.split("")
-  #   word_array_two = @secondword.downcase.split("")
-  #   sorted_one = word_array_one.sort do |a,b| a <=> b end
-  #   sorted_two = word_array_two.sort do |a,b| a <=> b end
-  #   both_arrays_cloned = (sorted_one.clone + sorted_two.clone)
-  #     print both_arrays_cloned
-  #     print sorted_two
-  #     print sorted_one
-  #   both_arrays_cloned.each do |y|
-  #     if y == ("a"|| "e"|| "i"|| "o"|| "u"|| "y")
-  #       print y
-  #       vari ="not a word, try again"
-  #     elsif sorted_one.join("") == sorted_two.join("")
-  #       return "anagram"
-  #     elsif sorted_one.join("") != sorted_two.join("")
-  #       "antigram"
-  #     end
-  #   end
-  # end
+
+# def compare
+#
+#   word_array_one = @firstword.downcase.split("")
+#   word_array_two = @secondword.downcase.split("")
+#   sorted_one = word_array_one.sort do |a,b| a <=> b end
+#   sorted_two = word_array_two.sort do |a,b| a <=> b end
+#   both_arrays_cloned = (sorted_one.clone + sorted_two.clone)
+#     # print both_arrays_cloned
+#     # print sorted_two
+#     # print sorted_one
+#   both_arrays_cloned.each do |y|
+#     if y == ("a"||"e"||"i"||"o"||"u"||"y")
+#       print y
+#       "not a word, try again"
+#       word_array_twoanswer = true
+#     else
+#       array_answer = false
+#     end
+#     @answer = array_answer
+#   end
+#   if @answer == true && sorted_one.join("") == sorted_two.join("")
+#     "anagram"
+#   elsif @answer == true && sorted_one.join("") != sorted_two.join("")
+#       "antigram"
+#   elseif @answer == false
+#     "not a word"
+#   end
+#
+# end
+# end
+
+
   def compare
 
     word_array_one = @firstword.downcase.split("")
@@ -32,31 +44,67 @@ class Anagram
     sorted_one = word_array_one.sort do |a,b| a <=> b end
     sorted_two = word_array_two.sort do |a,b| a <=> b end
     both_arrays_cloned = (sorted_one.clone + sorted_two.clone)
-      print both_arrays_cloned
-      print sorted_two
-      print sorted_one
 
-    both_arrays_cloned.each do |y|
-      if y == ("a"|| "e"|| "i"|| "o"|| "u"|| "y")
-        print y
-        @is_a_word = true
-        @is_a_word
-      else
-        @is_a_word = false
-        @is_a_word
-      end
-      @is_a_word
-    end
-    if @is_a_word == true
+    # total_length = both_arrays_cloned.length
+    # is_word = (0..total_length).detect {|i| i == ("a"||"e"||"i"||"o"||"u"||"y")}
+    # print is_word
+    # both_arrays_cloned.each do |y|
+    #    y == ("a"|| "e"|| "i"|| "o"|| "u"|| "y")
+    #     print y
+    #     "not a word, try again"
+    #     answer = true
+     is_word = both_arrays_cloned.any?{|i| i == ("a"||"e"||"i"||"o"||"u"||"y")}
+     print "has a vowel"
+     print is_word
+    if is_word == true
       if sorted_one.join("") == sorted_two.join("")
-        return "anagram"
-      else sorted_one.join("") != sorted_two.join("")
-        "antigram"
+      answer ="anagram"
+      elsif sorted_one.join("") != sorted_two.join("")
+      answer ="antigram"
       end
-    else @is_a_word == false
-      "not a word, try again"
+    elsif is_word == false
+    answer = "not a word, try again"
     end
-
-
+    puts answer
+    answer
   end
 end
+#   def compare
+#
+#     word_array_one = @firstword.downcase.split("")
+#     word_array_two = @secondword.downcase.split("")
+#     sorted_one = word_array_one.sort do |a,b| a <=> b end
+#     sorted_two = word_array_two.sort do |a,b| a <=> b end
+#     both_arrays_cloned = (sorted_one.clone + sorted_two.clone)
+#       print both_arrays_cloned
+#       print sorted_two
+#       print sorted_one
+#     def has_a_vowel
+#       both_arrays_cloned.each do |y|
+#         if y == ("a"|| "e"|| "i"|| "o"|| "u"|| "y")
+#           print y
+#           is_a_word = true
+#         else
+#           is_a_word = false
+#         end
+#       end
+#       @yes_vowel = is_a_word
+#     end
+#     if @yes_vowel == true
+#       if sorted_one.join("") == sorted_two.join("")
+#         return "anagram"
+#         answer = "anagram"
+#       else sorted_one.join("") != sorted_two.join("")
+#         answer = "antigram"
+#       end
+#       answer
+#     elsif @yes_vowel == false
+#       answer = "not a word, try again"
+#     else
+#       answer = "wrong spot"
+#     end
+#     answer
+#
+#
+#   end
+# end
